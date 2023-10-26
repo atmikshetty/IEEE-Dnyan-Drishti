d79942d1956468430cc7692b7ef917bbb575e4b9
daniel-was-taken <danferreira376@gmail.com>
2023-10-26 21:18:49 +0530

    Changes to timer

--- js/countdowntimer.js
+++ js/countdowntimer.js
@@ -1,26 +1,43 @@
-const daysElement = document.querySelector("#days");
-const hoursElement = document.querySelector("#hours");
-const minutesElement = document.querySelector("#minutes");
-const secondsElement = document.querySelector("#seconds");
+(function () {
+  const second = 1000,
+        minute = second * 60,
+        hour = minute * 60,
+        day = hour * 24;
 
-// Get specific date and time in milliseconds from 1970 UTC
-let countDownDate = new Date("Aug 31, 2024").getTime();
+  //I'm adding this section so I don't have to keep updating this pen every year :-)
+  //remove this if you don't need it
+  let today = new Date(),
+      dd = String(today.getDate()).padStart(2, "0"),
+      mm = String(today.getMonth() + 1).padStart(2, "0"),
+      yyyy = today.getFullYear(),
+      nextYear = yyyy + 1,
+      dayMonth = "09/30/",
+      birthday = dayMonth + yyyy;
+  
+  today = mm + "/" + dd + "/" + yyyy;
+  if (today > birthday) {
+    birthday = dayMonth + nextYear;
+  }
+  //end
+  
+  const countDown = new Date(birthday).getTime(),
+      x = setInterval(function() {    
 
-setInterval(() => {
-  // Get today's date and time in milliseconds from 1970 UTC
-  let now = new Date().getTime();
+        const now = new Date().getTime(),
+              distance = countDown - now;
 
-  let distance = countDownDate - now;
+        document.getElementById("days").innerText = Math.floor(distance / (day)),
+          document.getElementById("hours").innerText = Math.floor((distance % (day)) / (hour)),
+          document.getElementById("minutes").innerText = Math.floor((distance % (hour)) / (minute)),
+          document.getElementById("seconds").innerText = Math.floor((distance % (minute)) / second);
 
-  let days = Math.floor(
-    (distance % (1000 * 60 * 60 * 24 * 30)) / (1000 * 60 * 60 * 24)
-  );
-  let hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-  let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-  let seconds = Math.floor((distance % (1000 * 60)) / 1000);
-
-  daysElement.textContent = days;
-  hoursElement.textContent = hours;
-  minutesElement.textContent = minutes;
-  secondsElement.textContent = seconds;
-}, 1000);
+        //do something later when date is reached
+        if (distance < 0) {
+          document.getElementById("headline").innerText = "It's my birthday!";
+          document.getElementById("countdown").style.display = "none";
+          document.getElementById("content").style.display = "block";
+          clearInterval(x);
+        }
+        //seconds
+      }, 0)
+  }());
